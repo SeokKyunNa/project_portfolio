feda0e8edea29ceba470451e5eabea887020de14
engsk <engsk1211@gmail.com>
2021-08-20 13:11:11 +0900

    react router 설정

--- client_react/src/App.js
+++ client_react/src/App.js
@@ -1,5 +1,6 @@
 import styled, { css } from 'styled-components';
 import UserSign from './UserSign/UserSign.jsx';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 const Container = styled.div`
   background-color: #dfe6ed;
@@ -9,7 +10,22 @@ const Container = styled.div`
 function App() {
   return (
     <Container>
-      <UserSign />
+      <BrowserRouter>
+        <Switch>
+          {/* 로그인 여부에 따라서 root path 상태 선택 (로그인 화면 or 메인 화면) */}
+          <Route exact Path="/">
+            <UserSign />
+          </Route>
+          {/* 로그인 화면 */}
+          <Route exact path="/signin" />
+          {/* 메인 화면 (내 정보) */}
+          <Route path="/myinfo" />
+          {/* 네트워크 화면 (사용자 목록) */}
+          <Route exact path="/userlist" />
+          {/* 유저 정보 화면 */}
+          <Route path="/userinfo/:id" />
+        </Switch>
+      </BrowserRouter>
     </Container>
   );
 }
